d13f20c051ec107ff2a31488a77b6ab097ca538a
Mariam22MR <138938363+Mariam22MR@users.noreply.github.com>
2025-01-18 00:19:05 +0300

    Create form.jsx

---
+++ src/components2/pages/form.jsx
@@ -0,0 +1,28 @@
+            <div
+            style={{
+            backgroundImage: `url(${img2})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            minHeight: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            }}
+            >
+                <div
+                style={{
+                    backgroundColor: "rgba(255, 255, 255, 0.67)", // لون خلفية شبه شفاف
+                    borderRadius: "15px",
+                    padding: "30px",
+                    maxWidth: "600px",
+                    width: "50%",
+                    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+                    }}
+                >
+
+                    <h2 className="text-center mb-4" style={{ fontWeight: "bold" }}>
+                        Contact Us
+                    </h2>
+                    <Forms />
+                </div>
+            </div>
